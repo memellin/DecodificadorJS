9a8bc1ca5f788b895293831885d42bfd1bee7c6b
Enock <enocksm@outlook.com>
2024-02-19 23:08:15 -0300

    decodificando logica adcionada

--- decodificador.js
+++ decodificador.js
@@ -1,19 +1,51 @@
+const palavras = {
+    a: 'ai',
+    e: 'enter',
+    i: 'imes',
+    o: 'ober',
+    u: 'ufat'
+}
+const alphabet = ['a', 'e', 'i', 'o', 'u'];
+
 function codificar(){
     let word = document.getElementById("texto__original").value;
+    word = word.toLowerCase();
     let word2 = "";
     for(let i = 0; i < word.length; i++) {
-        word[i] === 'a' ? word2 += 'ai' : word[i];
-        word[i] === 'e' ? word2 += 'enter' : word[i];
-        word[i] === 'i' ? word2 += 'ines' : word[i];
-        word[i] === 'o' ? word2 += 'ober' : word[i];
-        word[i] === 'u' ? word2 += 'ufat' : word[i];
+        word[i] === alphabet[0] ? word2 += palavras.a : word[i];
+        word[i] === alphabet[1] ? word2 += palavras.e : word[i];
+        word[i] === alphabet[2] ? word2 += palavras.i : word[i];
+        word[i] === alphabet[3] ? word2 += palavras.o : word[i];
+        word[i] === alphabet[4] ? word2 += palavras.u : word[i];
     };
     document.getElementById("texto__criptografado").value = word2;
     console.log(word2);
 }
 
 function decodificar(){
-    
+    let word = document.getElementById("texto__criptografado").value;
+    word = word.toLowerCase();
+    let word2 = "";
+    let tempword = [word];
+    for(let i = 0; i < word.length; i++) {
+        let letraDecodificada = null;
+        // Verifica se a letra atual corresponde a alguma palavra criptografada
+        for (let letra in palavras) {
+            if (palavras[letra] === word.substring(i, i + palavras[letra].length)) {
+                letraDecodificada = letra;
+                break;
+            }
+        }
+        // Se a letra foi decodificada, adiciona ao resultado, senão, mantém a letra original
+        if (letraDecodificada !== null) {
+            word2 += letraDecodificada;
+            i += palavras[letraDecodificada].length - 1; // Pula a quantidade de caracteres da palavra criptografada
+        } else {
+            word2 += word[i];
+        }
+    }
+    document.getElementById("texto__criptografado").value = word2;
+    console.log(word2);
 }
 
 function copiar(){
